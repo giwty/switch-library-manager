b28f498e75de30f8df81d8d590b186a6d5d6ca75
giwty <0b@LonXm#fkv>
2020-10-05 21:04:20 -0700

    add file open retry

--- switchfs/splitFileReader.go
+++ switchfs/splitFileReader.go
@@ -2,6 +2,7 @@ package switchfs
 
 import (
 	"errors"
+	"github.com/avast/retry-go"
 	"io"
 	"io/ioutil"
 	"os"
@@ -20,6 +21,38 @@ type splitFile struct {
 	files     []ReadAtCloser
 	path      string
 	chunkSize int64
+}
+
+type fileWrapper struct {
+	file ReadAtCloser
+	path string
+}
+
+func NewFileWrapper(filePath string) (*fileWrapper, error) {
+	result := fileWrapper{}
+	result.path = filePath
+	file, err := _openFile(filePath)
+	if err != nil {
+		return nil, err
+	}
+	result.file = file
+	return &result, nil
+}
+
+func (sp *fileWrapper) ReadAt(p []byte, off int64) (n int, err error) {
+	if sp.file != nil {
+		return sp.file.ReadAt(p, off)
+	}
+	return 0, errors.New("file is not opened")
+}
+
+func (sp *fileWrapper) Close() error {
+
+	if sp.file != nil {
+		return sp.file.Close()
+	}
+
+	return nil
 }
 
 func NewSplitFileReader(filePath string) (*splitFile, error) {
@@ -51,7 +84,7 @@ func (sp *splitFile) ReadAt(p []byte, off int64) (n int, err error) {
 	}
 
 	if len(sp.files) == 0 || sp.files[part] == nil {
-		file, _ := os.Open(path.Join(sp.path, sp.info[part].Name()))
+		file, _ := _openFile(path.Join(sp.path, sp.info[part].Name()))
 		sp.files[part] = file
 	}
 	off = off - sp.chunkSize*int64(part)
@@ -60,6 +93,19 @@ func (sp *splitFile) ReadAt(p []byte, off int64) (n int, err error) {
 		return 0, errors.New("offset is out of bounds")
 	}
 	return sp.files[part].ReadAt(p, off)
+}
+
+func _openFile(path string) (*os.File, error) {
+	var file *os.File
+	var err error
+	retry.Attempts(5)
+	err = retry.Do(
+		func() error {
+			file, err = os.Open(path)
+			return err
+		},
+	)
+	return file, err
 }
 
 func (sp *splitFile) Close() error {
@@ -76,6 +122,6 @@ func OpenFile(filePath string) (ReadAtCloser, error) {
 	if _, err := strconv.Atoi(filePath[len(filePath)-1:]); err == nil {
 		return NewSplitFileReader(filePath)
 	} else {
-		return os.Open(filePath)
+		return NewFileWrapper(filePath)
 	}
 }
